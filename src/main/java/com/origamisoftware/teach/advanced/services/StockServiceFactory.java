ee2f8991c672660c0b56774ebbe4270f985fa8f9
John Roseberry <lmrjrr@comcast.net>
2019-03-02 10:11:30 -0500

    initial commit

---
+++ src/main/java/com/origamisoftware/teach/advanced/services/StockServiceFactory.java
@@ -0,0 +1,20 @@
+package com.origamisoftware.teach.advanced.services;
+
+/**
+ * A factory that returns a <CODE>StockService</CODE> instance.
+ */
+public class StockServiceFactory {
+
+    /**
+     * Prevent instantiations
+     */
+    private StockServiceFactory() {}
+
+    /**
+     *
+     * @return get a <CODE>StockService</CODE> instance
+     */
+    public static StockService getInstance() {
+        return new SimpleStockService();
+    }
+}
